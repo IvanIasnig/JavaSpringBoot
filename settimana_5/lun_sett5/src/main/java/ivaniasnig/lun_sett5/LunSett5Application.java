f26e7c0989103dbb3f2f5c9b167f5c309f037a15
ivan1998iasnig@gmail.com <ivan1998iasnig@gmail.com>
2023-07-24 16:15:46 +0200

    first commit

---
+++ settimana_5/lun_sett5/src/main/java/ivaniasnig/lun_sett5/LunSett5Application.java
@@ -0,0 +1,13 @@
+package ivaniasnig.lun_sett5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LunSett5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LunSett5Application.class, args);
+	}
+
+}
---
+++ settimana_5/lun_sett5/src/test/java/ivaniasnig/lun_sett5/LunSett5ApplicationTests.java
@@ -0,0 +1,13 @@
+package ivaniasnig.lun_sett5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LunSett5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

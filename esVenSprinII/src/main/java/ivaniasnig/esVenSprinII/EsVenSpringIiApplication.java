3b80c3c10414f928718f4fa3ba20dfd7aa00c3c4
ivan1998iasnig@gmail.com <ivan1998iasnig@gmail.com>
2023-08-04 15:34:43 +0200

    testati tutti metodi utente

--- esVenSprinII/src/main/java/ivaniasnig/esVenSprinII/EsVenSpringIiApplication.java
+++ esVenSprinII/src/main/java/ivaniasnig/esVenSprinII/EsVenSpringIiApplication.java
@@ -1,31 +1,20 @@
 package ivaniasnig.esVenSprinII;
 
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
-import ivaniasnig.esVenSprinII.utenti.Utente;
-import ivaniasnig.esVenSprinII.utenti.UtenteService;
 
 @SpringBootApplication
 public class EsVenSpringIiApplication {
 	
-//	@Autowired
-//	UtenteService utentiService;
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(EsVenSpringIiApplication.class, args);
 	}
 
-//	@Bean
-//	public CommandLineRunner initData() {
-//		return args -> {
-//			Utente user = new Utente("Mario", "Rossi", "ciao@gmail.com", "abc123");
-//			utentiService.save(user);
-//		};
-//	}
+
 }
 
